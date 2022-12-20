477a244b7086d674e43f0a71750a63719280098e
Iskar Maulana <lanaiskar@gmail.com>
2022-12-20 14:48:56 +0700

    Tambah main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,34 @@
 fun main(args: Array<String>) {
-
+    message()
 }
+
+fun message(){
+    do {
+        println("Aplikasi Kalkulator Sederhana")
+        println("=============================")
+        println("Pilih operasi yang akan digunakan")
+        println("1. Tambah")
+        println("2. Kurang")
+        println("3. Kali")
+        println("4. Bagi")
+        println("0. Selesai")
+        print("Pilih salah satu: ")
+        var pilih = readln().toInt()
+        if (pilih == 0){
+            println("Terimakasih")
+        }
+        print("Masukkan nilai ke 1: ")
+        var a = readln().toInt()
+        print("Masukkan nilai ke 2: ")
+        var b = readln().toInt()
+
+        when (pilih){
+            1 -> println("Hasil penambahan antara $a + $b adalah ${tambah(a, b)}")
+            2 -> println()
+            3 -> println()
+            4 -> println()
+        }
+    }while (pilih !=0)
+}
+
+fun tambah(a: Int, b:Int) = a+b;
